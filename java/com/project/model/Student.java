fed1289c6138342f1f1d221e75cd3958dd02041a
Girija35 <120115243+Girija35@users.noreply.github.com>
2023-03-28 10:09:32 +0530

    Add files via upload

---
+++ java/com/project/model/Student.java
@@ -0,0 +1,47 @@
+package com.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Student_Detail")
+public class Student {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String phoneno;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+    
+}
